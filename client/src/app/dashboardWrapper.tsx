281db457b86bf7e794067afc5c117e553609f072
daniel <dany20082011@gmail.com>
2026-03-02 21:30:17 +0200

    prisma and database installition

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { use, useEffect } from "react";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+    useEffect(() => {
+      if(isDarkMode) {
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.add("dark");
+      } else {        
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("light");
+      }
+    }, [isDarkMode]);  
+  return (
+    <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default function DashboardWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <StoreProvider>
+      <DashboardLayout>
+        {children}
+      </DashboardLayout>
+    </StoreProvider>
+  );
+}
---
+++ client/src/state/api.ts
@@ -0,0 +1,9 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+    reducerPath: "api",
+    endpoints: (builder) => ({}),
+})
+
+export const {} = api;
