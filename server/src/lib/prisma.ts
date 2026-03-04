1d499da8f0df209e9ed6097533eb918cb7682381
daniel <dany20082011@gmail.com>
2026-03-04 21:25:36 +0200

    added validation for the database url

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -2,9 +2,11 @@ import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is required");
+}
 const adapter = new PrismaPg({ connectionString });
-
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
